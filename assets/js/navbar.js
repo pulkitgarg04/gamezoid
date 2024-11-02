63c7f5e4dc80d69c67a24d51c513fc4ea02bd8a0
Pulkit Garg <pulkitgargbnl@gmail.com>
2024-11-02 16:19:42 +0530

    re-initialized project

--- assets/js/navbar.js
+++
@@ -1,9 +0,0 @@
-function accountRoute() {
-    const currentUser = localStorage.getItem('currentUser');
-    
-    if (currentUser) {
-        window.location.href = './account.html';
-    } else {
-        window.location.href = './login.html';
-    }
-};
